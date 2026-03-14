9ea45d93ff59f014e0fc75b9a7ceea16fec3abe6
Semen Rybkin <alexcemen99@gmail.com>
2026-03-14 23:24:52 +0700

    feat: add logoUrl to CoinData domain model

--- app/src/main/kotlin/com/alexcemen/cryptoportfolio/domain/model/CoinData.kt
+++ app/src/main/kotlin/com/alexcemen/cryptoportfolio/domain/model/CoinData.kt
@@ -4,6 +4,7 @@ data class CoinData(
     val symbol: String,
     val priceUsdt: Double,
     val quantity: Double,
+    val logoUrl: String? = null,
 ) {
     val totalPositionUsdt: Double get() = priceUsdt * quantity
 }
